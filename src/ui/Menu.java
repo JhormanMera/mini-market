125506be10f3d2dedc5e4494ca9e14776847cfda
JhormanMera <jhorman.mera1@correo.icesi.edu.co>
2021-04-08 22:15:25 -0500

    second commit

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -2,6 +2,8 @@ package ui;
 
 import java.util.Scanner;
 
+import exception.AgeException;
+import exception.DayException;
 import model.Minimarket;
 
 public class Menu {
@@ -36,32 +38,19 @@ public class Menu {
 	}
 	public void addPerson() {
 		System.out.println("Tipo De Documento de Identidad");
-		System.out.println("(1) Tarjeta De Identidad"+"\n"+"(2) Cedula De Ciudadania"+"\n"+"(3) Pasaporte"+"\n"+"(4) Cedula De Extranjeria");
-		int choice = sc.nextInt();
-		sc.nextLine();
-		String idType="";
-		switch(choice) {
-		case 1:
-			idType="Tarjeta De Identidad";
-			break;
-		case 2:
-			idType="Cedula De Ciudadania";
-			break;
-		case 3:
-			idType="Pasaporte";
-			break;
-		case 4:
-			idType="Cedula De Extranjeria";
-			break;
-		default:
-			System.out.println("Opcion invalida, repita nuevamente");
-	}
+		System.out.println("TI -Tarjeta de Identidad"+"\n"+"CC - Cédula de Ciudadanía"+"\n"+"PP -Pasaporte"+"\n"+"CE -Cédula de Extranjería");
+		String choice = sc.nextLine().toUpperCase();
 		System.out.println("Ingrese el numero de documento (Sin puntos)");
 		int id = sc.nextInt();
-		sc.nextLine();
-		
-		if(minimarket.addPerson(idType,id)) {
-			System.out.println("\n"+"***Ingreso agregado correctamente***");
+		sc.nextLine();		
+		try {
+			if(minimarket.addPerson(choice,id)) {
+				System.out.println("\n"+"***Ingreso agregado correctamente***");
+			}
+		} catch (AgeException e) {
+			e.printStackTrace();
+		} catch(DayException e) {
+			e.printStackTrace();
 		}
 	}
 	public void doOperation(int choice) {
